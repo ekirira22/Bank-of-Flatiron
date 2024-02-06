7d39938f60dc4e9890845067a9c4a848bbac19bb
Eric Maranga <ekirira22@gmail.com>
2024-02-06 13:34:59 +0300

    configured tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className='App'>
-      
+      <h1 className='App-header pt-10'>BANK OF FLATIRON</h1>
     </div>
   );
 }
